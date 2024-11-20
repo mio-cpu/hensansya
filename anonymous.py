9f6cc7ec01f9995a40b6f9a2217b49ecf1d90db7
mio-cpu <anyaazazamasu@gmail.com>
2024-11-21 05:37:30 +0900

    Update anonymous.py

--- anonymous.py
+++ anonymous.py
@@ -4,14 +4,14 @@ from discord.ext import commands
 class AnonymousMessages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.anonymous_channel_id = 123456789012345678  # 目安箱のチャンネルID
+        self.anonymous_channel_id = 1308544883899764746  # 目安箱チャンネルのIDを設定
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.id != self.anonymous_channel_id or message.author.bot:
-            return
+            return  # BOTや対象外のチャンネルのメッセージは無視
 
-        await message.delete()
+        await message.delete()  # 元のメッセージを削除
 
         embed = discord.Embed(
             title="匿名メッセージ",
@@ -19,7 +19,7 @@ class AnonymousMessages(commands.Cog):
             color=discord.Color.blurple()
         )
         embed.set_footer(text="目安箱より")
-        await message.channel.send(embed=embed)
+        await message.channel.send(embed=embed)  # 匿名メッセージとして送信
 
 async def setup(bot):
     await bot.add_cog(AnonymousMessages(bot))
